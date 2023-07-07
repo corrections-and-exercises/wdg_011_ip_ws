f12f8121d3ab0e2cb7e59982cf44f9dd507a90c3
patrick-plue <patrick.pluequett@wbscodingschool.com>
2023-07-07 15:39:47 +0200

    add map

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Ip from './components/Ip.jsx';
+import Map from './components/Map.jsx';
 
 function App() {
   return (
     <>
-      <Ip />
+      {/* <Ip /> */}
+      <Map />
     </>
   );
 }
