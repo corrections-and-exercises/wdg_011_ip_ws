afd4e57b021781b58dd933c7290a57165ce73c8d
patrick-plue <patrick.pluequett@wbscodingschool.com>
2023-07-07 15:18:25 +0200

    add ip

--- src/components/Ip.jsx
+++ src/components/Ip.jsx
@@ -1,5 +1,33 @@
+import {useState, useEffect} from 'react';
+
 function Ip() {
-  return <div>Ip</div>;
+  const [ipData, setIpData] = useState();
+  const [ipError, setIpError] = useState();
+
+  const apiKey = import.meta.env.VITE_IPIFY_KEY;
+
+  const url = `https://geo.ipify.org/api/v2/country?apiKey=${apiKey}`;
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(url);
+        if (!res.ok)
+          throw new Error(`Request failed ${res.status} ${res.statusText}`);
+        const data = await res.json();
+        setIpData(data);
+      } catch (error) {
+        setIpError(error);
+      }
+    }
+    fetchData();
+  }, [url]);
+
+  if (ipError) {
+    return <p>Something went wrong {ipError.message}</p>;
+  }
+
+  return <div>{ipData && ipData.ip}</div>;
 }
 
 export default Ip;
